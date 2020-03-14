842e256b28eeffee91cd3ff6966ac8ee2f9b02e8
Steven Vuong <steven@hackpartners.com>
2020-03-14 21:49:49 +0000

    initialised augment py

---
+++ scratch_scripts/code_samples/augment_data.py
@@ -0,0 +1,12 @@
+"""
+Goal of this file is to augment our training data.
+Do this of our training images.
+
+We have one test image to benchmark against, 0 or 1 result.
+
+Ref: https://www.tensorflow.org/tutorials/images/data_augmentation
+
+Side detourr
+"""
+
+from PIL import Image
