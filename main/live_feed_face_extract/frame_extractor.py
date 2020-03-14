1aca34d23e1ca10b0dd3a672a3cfbadfa7fea918
Steven Vuong <steven@hackpartners.com>
2020-03-14 18:56:38 +0000

    improved docstring

--- main/live_feed_face_extract/frame_extractor.py
+++ main/live_feed_face_extract/frame_extractor.py
@@ -1,6 +1,11 @@
 """
 Extracts frames from youtube live streams
-Ref: https://opencv-python-tutroals.readthedocs.io/en/latest/py_tutorials/py_gui/py_video_display/py_video_display.html
+
+Todo:
+    - Add tests
+
+Ref: 
+https://opencv-python-tutroals.readthedocs.io/en/latest/py_tutorials/py_gui/py_video_display/py_video_display.html
 """
 import cv2
 import pafy
