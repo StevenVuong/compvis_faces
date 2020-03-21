797c48d517b3b847fe3010bae3efd7c4b09f2f90
Steven Vuong <steven@hackpartners.com>
2020-03-21 18:17:43 +0000

    implemented progressbar

--- main/face_sentiment_analysis/utils/preprocessing.py
+++ main/face_sentiment_analysis/utils/preprocessing.py
@@ -1,9 +1,20 @@
 import pandas as pd
 import numpy as np
+import os
 import configparser
+import logging
+from progressbar import ProgressBar, Percentage, Bar
 
 config = configparser.ConfigParser()
 config.read("../config.ini")
+
+# Libs from other dirs
+import sys
+sys.path.append("../..")
+from libs import log
+
+logger = logging.getLogger("Preprocessing")
+
 
 BASE_DIR = config["emotion-classify"]["DATA_BASEPATH"]
 DATA_CSVPATH = os.path.join(BASE_DIR, "fer2013/fer2013.csv")
@@ -11,28 +22,51 @@ WIDTH, HEIGHT = np.repeat(
     config.getint("emotion-classify", "IMG_WIDTH_HEIGHT"), 
     2)
 
-data = pd.read_csv(TRAIN_CSVPATH)
-datapoints = data['pixels'].tolist()
 
-#getting features for training
-X = []
-for xseq in datapoints:
-    xx = [int(xp) for xp in xseq.split(' ')]
-    xx = np.asarray(xx).reshape(width, height)
-    X.append(xx.astype('float32'))
+def main():
 
-X = np.asarray(X)
-X = np.expand_dims(X, -1)
+    logger.info("Starting Preprocessing")
 
-#getting labels for training
-# y = pd.get_dummies(data['emotion']).as_matrix()
+    data = pd.read_csv(DATA_CSVPATH)
+    pixels_lists = data['pixels'].tolist()
+
+    # Get features for training
+    X = []
+    pbar = ProgressBar(widgets=[Percentage(), Bar(">")])
+    for pixels_list in pbar(pixels_lists):
+        # Parse string into xy array
+        xx = [int(pixels_list) for pixels_list in pixels_list.split(' ')]
+        xx = np.asarray(xx).reshape(WIDTH, HEIGHT)
+        X.append(xx.astype('float32'))
+
+    X = np.asarray(X)
+    X = np.expand_dims(X, -1)
+
+    # Get labels for training (one-hot encoded)
+    y = pd.get_dummies(data['emotion']).values
+    print(y[0])
+
+    print(X.shape)
 
 # #storing them using numpy
 # np.save('fdataX', X)
 # np.save('flabels', y)
 
-# print("Preprocessing Done")
-# print("Number of Features: "+str(len(X[0])))
-# print("Number of Labels: "+ str(len(y[0])))
-# print("Number of examples in dataset:"+str(len(X)))
-# print("X,y stored in fdataX.npy and flabels.npy respectively")
+    logger.info("Preprocessing Done")
+    logger.info(f"Number of examples in dataset: {str(len(X))}")
+    logger.info(f"Number of Features: {str(len(X[0]))}")
+    logger.info(f"Number of Labels: {str(len(y[0]))}")
+    # print("X,y stored in fdataX.npy and flabels.npy respectively")
+
+if __name__=="__main__":
+
+    logger.setLevel(logging.INFO)
+    log.add_stream_handler(logger)
+
+    try:
+        main()
+
+    except Exception as error:
+        logger.exception("Unhandled exception:")
+        raise error
+
