c179a2da0d82262653b752d8c69a91c300cfe246
Steven Vuong <steven@hackpartners.com>
2020-03-22 13:18:03 +0000

    redo prerprrocessing; save images to the folder dir

--- main/face_sentiment_analysis/utils/preprocessing.py
+++ main/face_sentiment_analysis/utils/preprocessing.py
@@ -26,7 +26,7 @@ TRAIN_TEST_SPLIT = config.getfloat("emotion-classify", "TRAIN_TEST_SPLIT")
 WIDTH, HEIGHT = np.repeat(
     config.getint("emotion-classify", "IMG_WIDTH_HEIGHT"), 
     2)
-IMAGE_SAVE_DIR = os.path.join(BASE_DIR, "images/")
+IMAGE_SAVE_DIR = os.path.join(BASE_DIR, "images")
 DATA_CSVPATH = os.path.join(BASE_DIR, "fer2013/fer2013.csv")
 
 
@@ -34,19 +34,21 @@ def main():
 
     logger.info("Starting Preprocessing")
 
+    # Make directories to save, according to the label
     if not os.path.exists(IMAGE_SAVE_DIR):
-        os.makedirs(os.path.join(IMAGE_SAVE_DIR, "train"))
-        os.makedirs(os.path.join(IMAGE_SAVE_DIR, "test"))
+        for i in range(7):
+            os.makedirs(os.path.join(IMAGE_SAVE_DIR, "test", str(i)))
+            os.makedirs(os.path.join(IMAGE_SAVE_DIR, "train", str(i)))
 
     logger.info("Loading Data")
 
     data = pd.read_csv(DATA_CSVPATH)
 
     pixels_lists = data["pixels"].values
-    y = pd.get_dummies(data['emotion']).values
+    y = data['emotion'].values
 
     logger.info(f"Number of Samples in dataset: {len(pixels_lists)}")
-    logger.info(f"Number of Labels: {len(y[0])}")
+    logger.info(f"Number of Labels: {len(y)}")
 
     logger.info("Splitting data into train and test sets")
     pixels_lists_train, pixels_lists_test, y_train, y_test = train_test_split(
@@ -54,21 +56,19 @@ def main():
 
     logger.info(f"Training Size: {len(y_train)}, Test Size: {len(y_test)}")
 
-    # Save y-outputs
-    np.save(os.path.join(IMAGE_SAVE_DIR, "train", "y_train.npy"), y_train)
-    np.save(os.path.join(IMAGE_SAVE_DIR, "test", "y_test.npy"), y_test)
-
-    pbar = ProgressBar()
     logger.info("Converting and Saving Training Images")
+    pbar = ProgressBar()
     for idx, pixels_list in pbar(enumerate(pixels_lists_train)):
 
         # Parse string into xy array
         xx = [int(pixels_list) for pixels_list in pixels_list.split(' ')]
         xx = np.asarray(xx).reshape(WIDTH, HEIGHT)
 
+        emotion_label = y_train[idx]
+
         # Save as Image
         im = Image.fromarray(xx.astype("uint8"))
-        im.save(f"{IMAGE_SAVE_DIR}/train/thumb_{idx:04d}.png")
+        im.save(f"{IMAGE_SAVE_DIR}/train/{emotion_label}/thumb_{idx:04d}.png")
 
     pbar = ProgressBar()
     logger.info("Converting and Saving Test Images")
@@ -78,9 +78,11 @@ def main():
         xx = [int(pixels_list) for pixels_list in pixels_list.split(' ')]
         xx = np.asarray(xx).reshape(WIDTH, HEIGHT)
 
+        emotion_label = y_test[idx]
+
         # Save as Image
         im = Image.fromarray(xx.astype("uint8"))
-        im.save(f"{IMAGE_SAVE_DIR}/test/thumb_{idx:04d}.png")
+        im.save(f"{IMAGE_SAVE_DIR}/test/{emotion_label}/thumb_{idx:04d}.png")
 
 
     logger.info("Done.")
