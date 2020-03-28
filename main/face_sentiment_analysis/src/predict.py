516dce519389f15ab116bca49ccea652dd078a3a
Steven Vuong <steven@hackpartners.com>
2020-03-28 10:41:59 +0000

    predicts.. predicts; adjust so outputs representation

--- main/face_sentiment_analysis/src/predict.py
+++ main/face_sentiment_analysis/src/predict.py
@@ -1,26 +1,69 @@
-"""
-Mainly to store how to do the prediction; To flesh out if desired
-"""
+# Standard Imports
+import logging
+import numpy as np
+import configparser
+import os
+
+# Tensorflow Imports
+import tensorflow as tf
+from tensorflow.keras.preprocessing import image
+
+# Libs from other dirs
+import sys
+sys.path.append("../..")
+from libs import log
+
+logger = logging.getLogger("PREDICT")
+
+config = configparser.ConfigParser()
+config.read("../config.ini")
+
+BASE_DIR = config["emotion-classify"]["DATA_BASEPATH"]
+PRED_DIR = os.path.join(BASE_DIR, "images", "predict")
+
+WIDTH, HEIGHT = np.repeat(
+    config.getint("emotion-classify", "IMG_WIDTH_HEIGHT"), 
+    2)
 
 
-STEP_SIZE_TEST=test_generator.n//test_generator.batch_size
-# Must reset test generator before call pred
-# Otherwise get outputs in a weird order
-test_generator.reset() 
-pred=model.predict_generator(test_generator,
-steps=STEP_SIZE_TEST,
-verbose=1)
+def main():
 
-# Predicted indices have the predictions
-predicted_class_indices=np.argmax(pred,axis=1)
+    logger.info("Starting " + __name__)
 
-# Map to Original ID's to see what we predicted for what
-labels = (train_generator.class_indices)
-labels = dict((v,k) for k,v in labels.items())
-predictions = [labels[k] for k in predicted_class_indices]
+    logger.info("Loading pre-trained TF CNN Model")
+    model =  tf.keras.models.load_model("./model.h5")
 
-# Save to CSV
-filenames=test_generator.filenames
-results=pd.DataFrame({"Filename":filenames,
-                      "Predictions":predictions})
-results.to_csv("results.csv",index=False)
+    logger.info("Evaluating Results")
+    for img_name in os.listdir(PRED_DIR):
+
+        if not img_name.endswith((".jpg", ".png", ".jpeg")):
+            continue
+
+        logger.info(f"Loading Image {img_name}")
+
+        img_path = os.path.join(PRED_DIR, img_name)
+
+        img = image.load_img(img_path, target_size=(WIDTH, HEIGHT), grayscale=True)
+        img = image.img_to_array(img)/255
+        img = np.expand_dims(img, axis=0)
+
+        logger.info(f"Predicting Image {img_name}")
+        result = model.predict(img, batch_size=1)
+        result = np.argmax(result)
+
+        print(result)
+
+    logger.info("Done")
+
+
+if __name__=="__main__":
+
+    logger.setLevel(logging.INFO)
+    log.add_stream_handler(logger)
+
+    try:
+        main()
+
+    except Exception as error:
+        logger.exception("Unhandled exception:")
+        raise error
